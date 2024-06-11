6ee1f876442ebb18e270aa9fe429d6191928887f
PancakeTAS <pancake@mgnet.work>
2024-06-12 00:22:13 +0200

    feat: header base

---
+++ src/launchpadmk2.h
@@ -0,0 +1,26 @@
+/// \file launchpadmk2.h generic launchpad mk2 driver
+
+// (make sure to define LAUNCHPAD_IMPL in one of the source files)
+
+#ifndef LAUNCHPAD_H
+#define LAUNCHPAD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+
+} launchpad_t; //!< launchpad device handle
+
+#ifndef LAUNCHPAD_IMPL
+
+#else
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LAUNCHPAD_H
